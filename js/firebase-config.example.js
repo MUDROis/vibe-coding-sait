c133933bb6248bb571a9c49de2af750df5743809
MUDROis <>
2026-07-14 16:05:58 +0000

    Update from https://github.com/MUDROis/vibecoding-files/commit/e16ed93b0829323c225aa0bbfb1afe0cfa2f99b5

---
+++ js/firebase-config.example.js
@@ -0,0 +1,1 @@
+let firebaseConfig={apiKey:"YOUR_API_KEY",authDomain:"YOUR_PROJECT.firebaseapp.com",databaseURL:"https://YOUR_PROJECT-default-rtdb.REGION.firebasedatabase.app",projectId:"YOUR_PROJECT",storageBucket:"YOUR_PROJECT.firebasestorage.app",messagingSenderId:"YOUR_SENDER_ID",appId:"YOUR_APP_ID",measurementId:"YOUR_MEASUREMENT_ID"};firebase.initializeApp(firebaseConfig);
